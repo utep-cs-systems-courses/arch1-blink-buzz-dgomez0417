26346e994db32a3bb830893bc55c892d69da942f
dgomez0417 <gmdenies@miners.utep.edu>
2021-03-30 01:40:09 -0600

    changed blink count and added buzz count

--- project/blink/wdInterruptHandler.c
+++ project/blink/wdInterruptHandler.c
@@ -4,8 +4,14 @@
 void
 __interrupt_vec(WDT_VECTOR) WDT(){	/* 250 interrupts/sec */
   static char blink_count = 0;
-  if (++blink_count == 125) {
+  static char buzz_count = 0;
+  if (++blink_count == 166) {
     state_advance();
     blink_count = 0;
+  }
+
+  if (++buzz_count == 166){
+    buzzer_state_advance();
+    buzz_count = 0;
   }
 }
