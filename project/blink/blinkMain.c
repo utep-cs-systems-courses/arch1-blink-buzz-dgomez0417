32694cf350fc03f167d57f1cbe82a8d90729f031
dgomez0417 <gmdenies@miners.utep.edu>
2021-03-30 01:26:42 -0600

    added switch/buzzer init and buzzer set period to 0

--- project/blink/blinkMain.c
+++ project/blink/blinkMain.c
@@ -2,10 +2,15 @@
 #include <msp430.h>
 #include "libTimer.h"
 #include "led.h"
+#include "buzzer.h"
+#include "switches.h"
 
 int main(void) {
   configureClocks();		/* setup master oscillator, CPU & peripheral clocks */
   led_init();
+  switch_init();
+  buzzer_init();
+  buzzer_set_period(0);         /*stats the buzz*/
   enableWDTInterrupts();	/* enable periodic interrupt */
 
   or_sr(0x18);		/* CPU off, GIE on */
