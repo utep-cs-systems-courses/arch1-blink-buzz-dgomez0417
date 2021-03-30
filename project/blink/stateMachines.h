100cb43dbc4789500d571c956c18cd8c7000a660
dgomez0417 <gmdenies@miners.utep.edu>
2021-03-30 01:34:03 -0600

    added method for all 4 buttons

--- project/blink/stateMachines.h
+++ project/blink/stateMachines.h
@@ -2,5 +2,9 @@
 #define stateMachine_included
 
 void state_advance();
-
+void buzzer_state_advance();
+void buzzer_state_1_advance();
+void buzzer_state_2_advance();
+void buzzer_state_3_advance();
+void buzzer_state_4_advance();
 #endif // included
